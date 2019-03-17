0440eb0be6fce4016ec51997d00314b19a1d2d59
itoaki <itoaki.planet@gmail.com>
2019-03-18 03:35:18 +0900

    test
    test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ server.listen(process.env.PORT || 3000);
 
 // サーバー起動確認
 server.get('/', (req, res) => {
-    res.send('Hello World from git hub');
+    res.send('Hello World');
 });
 
 // Botからメッセージに応答
